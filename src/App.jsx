ecd498fa657456597ad78028d86f1115ee7c07f2
fyozrs <mkostovska33@gmail.com>
2025-12-21 11:19:29 +0100

    Fix file path case for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./css/App.css"
 import "./css/TopicCard.css"
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/NavBar";
 import React, { useState } from "react";

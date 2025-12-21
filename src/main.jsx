45187ad3b1b7acbccd9bbd43080da138ff5ae8ba
fyozrs <mkostovska33@gmail.com>
2025-12-21 11:34:32 +0100

    fix routing for vercel doployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom"
-import './css/index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from "react-router-dom"
+// import './css/index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./css/App.css";
+import "./css/TopicCard.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
